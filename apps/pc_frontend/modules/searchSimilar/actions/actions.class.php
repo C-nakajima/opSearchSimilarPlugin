8cb89900be2a0cec89dc3a9aba0f61325be381db
C-nakajima <nakajima@tejimaya.com>
2013-07-17 12:03:31 +0900

    add apps

---
+++ apps/pc_frontend/modules/searchSimilar/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * searchSimilar actions.
+ *
+ * @package    OpenPNE
+ * @subpackage searchSimilar
+ * @author     Your name here
+ */
+class searchSimilarActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfWebRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
