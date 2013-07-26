a2e9baba1206ff70863f41b441c9188068013a51
C-nakajima <nakajima@tejimaya.com>
2013-07-26 12:05:41 +0900

    add

--- apps/pc_frontend/modules/searchSimilar/actions/components.class.php
+++ apps/pc_frontend/modules/searchSimilar/actions/components.class.php
@@ -4,27 +4,23 @@ class searchSimilarComponents extends sfComponents
 {
   public function executeSearchSimilarGadget(sfWebRequest $request)
   {
-    #自分のメンバーIDを取得
-    $this->memberId = $this->getUser()->getMember()->getId();
     #自分の所属するコミュニティをランダムに5件取得
-    $this->communities = Doctrine::getTable('Community')->retrievesByMemberId($this->memberId, 5, true);
+    $this->communities = Doctrine::getTable('Community')->retrievesByMemberId($this->getUser()->getMember()->getId(), 5, true);
     #ランダムに取得したコミュニティを検索用配列に保存
-    $this->communitiesId = '0';
+    $this->communitiesId = array();
     foreach ($this->communities as $value) {
-      $this->communitiesId = $this->communitiesId . ',' . $value['id'];
+      $this->communitiesId[] = $value['id'];
     }
     #自分が所属するコミュニティ2つ以上に所属するメンバーのメンバーIDをランダムに3件取得
-    $this->similars =  Doctrine_Query::create()
-                      # ->select('member_id, COUNT(id) as member_id')
-                       ->select('member_id')
-                       ->from('CommunityMember')
-                      # ->Where('is_pre = ?', '0')
-                      # ->andWhereNotIn('member_id', $this->memberId)
-                      # ->andWhereIn('community_id', array(1))
-                      # ->having('num_phonenumbers >= 2')
-                      # ->groupBy('member_id')
-                      # ->orderBy('random()')
-                      # ->limit(3)
-;
+    $this->similars =  Doctrine::getTable('CommunityMember')->createQuery()
+      ->select('member_id')
+      ->Where('is_pre = ?', false)
+      ->andWhereNotIn('member_id', $this->getUser()->getMember()->getId())
+      #->andWhereIn('community_id', $this->communitiesId)
+      ->having('COUNT(id) >= 2')
+      ->groupBy('member_id')
+      ->orderBy('random()')
+      ->limit(3)
+      ->execute(array(), Doctrine::HYDRATE_NONE);
   }
 }
--- apps/pc_frontend/modules/searchSimilar/templates/_searchSimilarGadget.php
+++ apps/pc_frontend/modules/searchSimilar/templates/_searchSimilarGadget.php
@@ -1,2 +1,12 @@
+検索したコミュニティのコミュニティID:
 <?php
-echo $communitiesId;
+foreach ($communitiesId as $value) {
+  echo $value . ',';
+}
+?>
+<br>
+2つ以上の同じコミュニティに参加しているメンバーのメンバーID:
+<?php
+foreach ($similars as $value) {
+  echo $value[0] . ',';
+}
